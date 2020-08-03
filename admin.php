be9b2ef9dfe6335808942b2d145b13ead971a2a0
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-08-03 15:48:50 +0200

    Affichage des mots contenus dans mots.txt

---
+++ admin.php
@@ -0,0 +1,34 @@
+<?php
+# Gestion de la liste des mots
+# Ajouter - Supprimer - Afficher
+
+
+$fichier = fopen('mots.txt', 'r+');
+
+// Affichage de toutes les lignes du fichier
+
+if ($fichier) {
+    while (($ligne = fgets($fichier)) !== false) {
+        echo $ligne . "<br>";
+    }
+
+    fclose($fichier);
+}
+
+ ?>
+ <html>
+   <head>
+     <title> Liste de mots </title>
+   </head>
+   <body>
+     <!-- Afficher les mots se trouvant dans le fichier -->
+     <form action="" method="POST">
+       <label for "add_word"> Ajouter un mot </label>
+       <input type="text" name="add_word">
+     </form>
+
+     <form action="" method="POST">
+       <button name="delete_word"> Supprimer un mot </button>
+     </form>
+
+   </body>
