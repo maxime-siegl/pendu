27ba07f17564acfba12563af391a2494e359038d
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-08-03 11:30:50 +0200

    Suppression de commentaires + ajout bouton reset pour quitter la session

--- index.php
+++ index.php
@@ -5,15 +5,6 @@ $mot = "testeur";
 # Compter le nombre de caractères dans $mot
 $nombre_lettres = strlen($mot);
 $lettres = str_split($mot, 1);
-# Test : remplacer des lettres puis les retrouver
- /* $translate = strtr($mot, "e", "_");
-echo $translate . "<br>";
-
-$back_to_normal = strtr($translate, "_", "e");
-echo $back_to_normal . "<br>"; */
-
-
-
 
 /********/
 
@@ -49,14 +40,13 @@ if(isset($_POST["reponse"])){
     if(isset($_POST["reponse"])){ # Si le formulaire est utilisé
       # on compte combien de valeurs contient le tableau
 
-      if(count($false) < 5)
+      if(count($false) < 8)
       { # si le nombre de valeurs du tableau est inférieur à 5, on ajoute la valeur
         array_push($false, $_POST['reponse']); # On insère dans le tableau ce qui provient du formulaire
-      }else{ # si le tableau contient 5 valeurs, on affiche un message sans remplir le nouveau le tableau
+      }else{ # si le tableau contient 8 valeurs, on affiche un message sans remplir le nouveau le tableau
         echo "You failed.";
       }
     }
-
     $_SESSION["false"] = $false; # Enfin, on stocke dans la session le tableau nouvellement rempli
 
     var_dump($_SESSION["false"]);
@@ -64,28 +54,28 @@ if(isset($_POST["reponse"])){
 }
 
 # *********************************
-/* Faire 2 tableaux, l'un  comprenant le nombre de _ et l'autre comprenant les lettres du mot
-il s'agit ensuite de chercher dans le bon index du tableau _
-et d'afficher la lettre venant du seconde tableau, ou de post, à la bonne place */
-# Faire un tableau comprenant autant de _ que de lettres dans le mot
 
+# Remplit un tableau de _ avec le nombre de lettre du $mot
 $underscore_array = array_fill(0, strlen($mot), "_");
 var_dump($underscore_array);
 
+# Affiche les _
+echo "<br> Hidden word: <br>";
 foreach($underscore_array as $element){ # Chaque $element est une lettre
 
   echo $element . " ";
 }
 
-$lettres = str_split($mot, 1); # Les caractères sont stockés dans un tableau
-var_dump($lettres);
+# Les caractères du $mot sont stockés dans un tableau
+# echo "<br> Stockage des lettres dans un tableau : <br>";
+# $lettres = str_split($mot, 1);
+# var_dump($lettres);
 
 # Afficher le mot en séparant les lettres
-foreach($lettres as $element){ # Chaque $element est une lettre
+/* foreach($lettres as $element){ # Chaque $element est une lettre
 
   echo $element . " ";
-}
-
+} */
 
 
 # Trouver la lettre envoyée par POST dans le $mot
@@ -94,6 +84,8 @@ if(isset($_POST['reponse'])){
 
   $reponse = $_POST['reponse'];
   $lettres = str_split($mot, 1);
+
+  var_dump($lettres);
 
   if($position_lettre !== false){
     # Permet de remplacer les _ par la bonne lettre au bon index
@@ -107,7 +99,7 @@ if(isset($_POST['reponse'])){
         $underscore_array[$i] = $reponse;
       }
     }
-    var_dump($underscore_array);
+    var_dump($underscore_array); # l'un des e ne s'ajoute pas dans le tableau
     $_SESSION['letter_found'] = $underscore_array;
     #echo "letter is found and start at $position_lettre";
   }
@@ -129,7 +121,9 @@ puis dévoiler les lettres trouvées */
 /* Si l'user choisit une lettre de l'alphabet qui correspond à une lettre de $mot,
 on affiche la lettre correspondante */
 
-
+if(isset($_POST['reset'])){
+  session_destroy();
+}
 
 ?>
 <html>
@@ -141,4 +135,8 @@ on affiche la lettre correspondante */
       <label for "reponse"> Entrez une lettre : </label>
       <input type="text" name="reponse">
     </form>
+    <form action="" method="POST">
+      <button class="reset"> Reset </button>
+    </form>
+
   </body>
