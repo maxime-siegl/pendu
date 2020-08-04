4c73315e6b96b99c626693d9b4dbb5fe423a916c
maxime <maxime.siegl@laplateforme.io>
2020-08-04 15:42:04 +0200

    .

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $fichier = file('mots.txt'); # Lit le fichier et renvoie le résultat dans un tableau
-    $mot = $fichier[array_rand($fichier)]; # choisit une ligne au hasard
-    # trim() permet de supprimer les guillemets en début et en fin de chaîne, le résultat est stocké dans une session
-    $_SESSION["mot"] = trim($mot);
-
-    var_dump($random);
-    var_dump($_SESSION['trimmed']);
-?>
