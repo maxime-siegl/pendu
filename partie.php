f450f3d26b100e209b6d94498a6e6d30b5d27d8e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-08-06 19:35:50 +0200

    Pendu POO

--- partie.php
+++ partie.php
@@ -2,8 +2,9 @@
 class Partie
 {
   private $word;
-  private $errors = false;
+  private $errors = 0;
   private $good_answers = false;
+  private $underscore_array;
 
   private $id_user; # Usage à spécifier ultérieurement
 
@@ -11,48 +12,50 @@ class Partie
     $this->searchWord();
   }
 
+  public function __get($word) {
+    return $this->word;
+  }
+
   # Cherche un mot au hasard dans le fichier txt
   public function searchWord(){
     $fichier = file('mots.txt');
-    if(isset($_POST["start"])){
-      $word = $fichier[array_rand($fichier)];
-      $trimmed = trim($word); # Récupère la valeur sans guillemet
-      $underscore_array = array_fill(0, strlen($trimmed), "_"); # remplace les lettres par des _
-      $_SESSION["start"] =  $trimmed;
-      $_SESSION["underscore"] = implode(" ", $underscore_array);
-    }
+    $this->word = trim($fichier[array_rand($fichier)]); # Récupère la valeur sans guillemet
+    $this->underscore_array = array_fill(0, strlen($this->word), "_"); # remplace les lettres par des _
+  }
+
+  public function afficheUnderscore() {
+    var_dump($this->word);
+    echo var_dump(implode(" ", $this->underscore_array)); # transforme en string le tableau
   }
 
   # Cherche si la lettre postée se trouve dans le mot... et la place au bon endroit ?
-  public function searchLetter(){
-    $word = $this->word;
+  public function searchLetter($reponse){
+    $lettres = str_split($this->word);
     $good_answer = false;
-    for($i = 0; $i < strlen($word); $i++)
+    for($i = 0; $i < strlen($this->word); $i++)
     {
       if($lettres[$i] == $reponse)
       {
-        $underscore_array[$i] = $reponse;
+        $this->underscore_array[$i] = $reponse;
         $good_answer = true;
       }
-  }
+    }
+
+    if($good_answer == false) {
+      $this->errors++;
+    }
 }
   # Compte le nombre d'erreurs
-  public function countErrors(){
-    $this->errors;
-    if($answer == false){
-      $_SESSION["erreurs"]++;
+  public function affichePendu(){
+    if($this->errors > 0) {
+      echo "<img src='img/pendu$this->errors.jpg'>"; # Affichage d'une image en fonction du nombre de l'erreur
+
     }
-    $erreur = $_SESSION['erreurs'];
-    echo "<img src='img/pendu$erreur.jpg'>"; # Affichage d'une image en fonction du nombre de l'erreur
   }
 
   # Remet à zéro le nombre de lettres trouvées et le nombre d'erreurs
   public function resetAll(){
-    unset($_SESSION["letter_found"]);
-    unset($_SESSION["erreurs"]);
-
-    unset($_SESSION["underscore"]);
-    unset($_SESSION["start"]);
+    unset($_SESSION["partie"]);
   }
 }
 ?>
---
+++ test.php
@@ -0,0 +1,60 @@
+<?php
+# Page permettant de tester la classe Partie
+require('Partie.php');
+
+session_start();
+
+if(isset($_SESSION["partie"])) {
+  $partie = $_SESSION["partie"];
+}
+
+if(isset($_POST["start"])){
+
+  $partie = new Partie; # il faudra faire en sorte de créer d'autres parties... peut-être en créant l'objet ds post start?
+  $_SESSION["partie"] = $partie;
+}
+
+# Remplacer les lettres en fonction de la réponse de l'user
+if(isset($_POST["reponse"])){
+  $partie->searchLetter($_POST["reponse"]); # Ne fonctionne pas encore...
+}
+
+if(isset($_POST["reset"])){
+  $partie->resetAll(); # l'objet créé dans la première condition est inaccessible
+}
+
+?>
+
+<html>
+  <head>
+    <title> Jeu du pendu </title>
+  </head>
+  <body>
+    <?php
+      if(!isset($_SESSION["partie"])) {
+        ?>
+        <form action="" method="POST">
+          <button name="start"> Commencer </button>
+        </form>
+        <?php
+      }
+    ?>
+
+
+    <?php
+      if(isset($_SESSION["partie"])) {
+
+        $partie->affichePendu();
+        $partie->afficheUnderscore();
+
+        ?>
+        <form action="" method="POST">
+          <label for "reponse"> Entrez une lettre : </label>
+          <input type="text" name="reponse">
+        </form>
+    <form action="" method="POST">
+      <button name="reset"> Reset </button>
+    </form>
+  <?php } ?>
+
+  </body>
