e93887c697f3954414f1dbd23633e0f05e25f206
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-08-09 10:05:53 +0200

    Suppression de mot dans le fichier

--- admin.php
+++ admin.php
@@ -1,20 +1,30 @@
 <?php
 # Gestion de la liste des mots
 # Ajouter - Supprimer - Afficher
+if(isset($_POST['add_word'])){ # Permet d'ajouter des mots
+  file_put_contents("mots.txt", $_POST['add_word'] . "\n", FILE_APPEND);
+  header("Location:admin.php");
+}
 
-$fichier = fopen('mots.txt', 'r+');
-# Pb de l'affichage en ligne dans le fichier mots.txt
 // Affichage de toutes les lignes du fichier
+$fichier = fopen('mots.txt', 'r+');
+$i = 0;
+while (($ligne = fgets($fichier)) !== false) { # Affiche la liste des mots
+  echo $ligne . "<a href='admin.php?mot=$i'>Supprimer un mot</a>"  ."<br>";
+  $i++;
+}
+fclose($fichier);
 
-if ($fichier) {
-    while (($ligne = fgets($fichier)) !== false) {
-        echo $ligne . "<a href='admin.php'>Supprimer un mot</a>"  ."<br>";
-    }
-    if(isset($_POST['add_word'])){
-      fwrite($fichier, $_POST['add_word'] . "\n" );
-      header("Location:admin.php");
-    }
-    fclose($fichier);
+/*** Supprimer un mot ***/
+if(isset($_GET["mot"]))
+{
+  $mot = $_GET["mot"];
+  $contents = file("mots.txt"); # On stocke le contenu du fichier dans $contents
+  unset($contents[$mot]);
+
+  file_put_contents("mots.txt", implode("\n", $contents)); # On met ce nouveau contenu dans le fichier
+
+  header("Location: admin.php");
 }
 
  ?>
